b404768f4dfadf60403f5a79ef0d38f8dac68937
parminder <parminder_jk@hotmail.com>
2015-03-14 00:13:43 -0700

    Added DAOs for higher and deeper views

--- js/parseData.js
+++ js/parseData.js
@@ -1,7 +1,53 @@
+var SERVER = "php/parseData.php";
+document.observe("dom:loaded", fetchFromToxicant);
 
 /**
  * Get all toxicants in the DB
- * @return {obj} 
+ * @return {obj}
+ */
+function getAllToxicants(){
+    alert("Ok!");
+    new Ajax.Request(SERVER, {
+        method : "POST",
+        parameters:{
+            action : "getToxicants"
+        },
+        onSuccess: function(ajax){
+            alert("P here");
+            var toxicants = JSON.parse(ajax.responseText);
+            console.log(toxicants);
+        },
+        onFailure: function(ajax){
+            alert("getAllToxicants : Unexpected error");
+        }
+    });
+}
+/**
+ * Get all toxicants in the DB
+ * @return {obj}
+ */
+function fetchFromToxicant(){
+    alert("Ok!");
+    new Ajax.Request(SERVER, {
+        method : "POST",
+        parameters:{
+            action : "fetchFromToxicant",
+            filter : 2317
+        },
+        onSuccess: function(ajax){
+            alert("P here");
+            var toxicants = JSON.parse(ajax.responseText);
+            console.log(toxicants);
+        },
+        onFailure: function(ajax){
+            alert("getAllToxicants : Unexpected error");
+        }
+    });
+}
+
+/**
+ * Get all toxicants in the DB
+ * @return {obj}
  * Formate for data=
  *	{
  *		name:"Toxicants",
@@ -12,25 +58,25 @@
  *	}
  */
 function getAllT(){
-	var cities = TAFFY();
-	$.ajax({
-	    url: "json/toxicants.json",
-	    success: function(data) {
-	        
-	        //data downloaded so we call parseJSON function 
-	        //and pass downloaded data
-	        var json = $.parseJSON(data);
-	        //now json variable contains data in json format
-	        //let's display a few items
-	        console.log(json);
-	    }
+    var cities = TAFFY();
+    $.ajax({
+        url: "json/toxicants.json",
+        success: function(data) {
+
+            //data downloaded so we call parseJSON function
+            //and pass downloaded data
+            var json = $.parseJSON(data);
+            //now json variable contains data in json format
+            //let's display a few items
+            console.log(json);
+        }
     });
-	//return data;
+    //return data;
 }
-		
+
 /**
  * Get all disease in the DB
- * @return {obj}  data 
+ * @return {obj}  data
  * Formate for data=
  *	{
  *		name:"Diseases",
@@ -38,17 +84,17 @@ function getAllT(){
  *			{name:"ADHD",size:"",ID:"" },
  *			{name:"",size:"",ID:"" }
  *		]
- *	} 
+ *	}
  */
 function getAllD(){
-	//return data;
+    //return data;
 }
 
 /**
  * Get filtered Toxicants list, filtered by tid and cid
  * @param  {array}	tid  // array of toxicant IDs which should be contained in data
- * @param  {array}	cid  // array of disease IDs. If one of memeber in tid does not have connection with cid at all, 
- * 						 // this toxicant should not be returned in data. 
+ * @param  {array}	cid  // array of disease IDs. If one of memeber in tid does not have connection with cid at all,
+ * 						 // this toxicant should not be returned in data.
  * @return {obj} data
  * Formate for data=
  *	{
@@ -60,7 +106,7 @@ function getAllD(){
  *	}
  */
 function getFilteredT(tid,cid){
-	//return data;
+    //return data;
 }
 
 /**
@@ -75,10 +121,10 @@ function getFilteredT(tid,cid){
  *			{name:"ADHD",size:"",ID:"" },
  *			{name:"",size:"",ID:"" }
  *		]
- *	} 
+ *	}
  */
 function getFilterdD(tid,cid){
-	//return data;
+    //return data;
 }
 
 function getTwithD(tid,cid){
@@ -86,5 +132,5 @@ function getTwithD(tid,cid){
 }
 
 function getDwithT(tid,cid){
-	
+
 }
