5852ab726eb0b1dae21262373fea8f19e56cf6a7
Amulya Velpula <amulyavelpula18@gmail.com>
2015-04-12 18:58:43 -0700

    Changed the Layout

--- index.php
+++ index.php
@@ -5,12 +5,18 @@
 ?>
 
 <div style="margin:0 auto;text-align:center;">
+	<p id="graphTitle">All diseases</p>
     <div id="selectRadio">
 	  <input type="radio" id="ADiseases" name="radio" checked="checked">
 	  <label for="ADiseases">By Disease</label>
 	  <input type="radio" id="AToxicants" name="radio" >
 	  <label for="AToxicants">By Toxicant</label>
-	  <span id="graphTitle">All diseases</span>
+	  <div class="searchList" >
+        <div id="searchSection">
+          <span>Search circle</span>
+          <select id='bubbleFilter' style="width:10%"></select>
+		</div>
+	</div>
     </div>
 </div>
 
@@ -20,13 +26,6 @@
 	
 	<div id="toxicantList">
     </div>
-	
-	<div class="searchList" >
-        <div id="searchSection">
-          <span>Search circle</span>
-          <select id='bubbleFilter' style="width:10%"></select>
-		</div>
-	</div>
 </div>	
 
 <div id="graph">
