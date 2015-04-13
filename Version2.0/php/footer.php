6c98e569c9cb2aeb49884447058d593b0e0b193e
parminder <parminder_jk@hotmail.com>
2015-04-12 17:11:21 -0700

    Minor code reformatting

--- Version2.0/php/footer.php
+++ Version2.0/php/footer.php
@@ -1,4 +1,10 @@
 <!--Common footer layout can come here-->
 
+<footer>
+    By MSS Students
+    Follow us on Facebook
+    <a href="https://www.facebook.com/groups/233141136866476/"><img src="css/images/fbicon.jpg" alt="Facebook" height="50" width="50"/></a>
+</footer>
+
     </body>
 </html>
