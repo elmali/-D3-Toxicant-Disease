67b4f96870b50e468f318564447c32458be18adb
hangmiao <hahnmiao@gmail.com>
2015-04-16 12:38:31 -0700

    Add php api.

---
+++ doc/api_php/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["f","fetchDiseaseReference()"],["f","fetchFromDisease()"],["f","fetchFromDiseaseCategory()"],["f","fetchFromToxicant()"],["f","getAllContaminants()"],["f","getAllDiseases()"],["f","getDiseaseCategories()"],["f","getFilterDiseasesByDC()"],["f","getFilterToxicantsByDC()"],["f","getTopDiseases()"],["f","getTopToxicants()"]];
